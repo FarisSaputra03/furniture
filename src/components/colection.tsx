cb38a43397476f8495113d77b275e132cd004417
Faris Saputra <farissaputra375@gmail.com>
2023-11-25 14:53:22 +0700

    update

--- src/components/colection.tsx
+++ src/components/colection.tsx
@@ -1,19 +1,21 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 export default function Colection() {
-  const targetDate = new Date('2024-12-31T23:59:59');
+  const targetDate = new Date("2024-12-31T23:59:59");
 
   const calculateTimeLeft = () => {
     const now = new Date();
-    const difference = targetDate .getTime() - now .getTime();
+    const difference = targetDate.getTime() - now.getTime();
 
     if (difference <= 0) {
       return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     }
 
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
@@ -46,64 +48,36 @@ export default function Colection() {
             </p>
           </div>
           <div className="flex md:flex-col">
-            <p className="font-size: 0.75rem">
+            <p className="font-size: 0.75rem w-[80%]">
               Lorem ipsum dolor sit amet, consectetur adipisicing sed do eiusmol
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
               mini veniam, quis nostrud exercitation ullamco laboris nisi ut
               aliquip eao commodo consequat Duis aute irure.
             </p>
           </div>
-          <div>
-            <div
-              className="flex items-center justify-start"
-              x-data="beer()"
-              x-init="start()"
-            >
-              <div>
-                <div className="text-6xl text-center flex w-full items-center justify-center">
-                  <div className=" mx-1 p-2 text-black rounded-lg">
-                    <div className="font-mono leading-none" x-text="days">
-                    {timeLeft.days}
-                    </div>
-                    <div className="font-mono uppercase text-sm leading-none">
-                      Days
-                    </div>
-                  </div>
-                  <div className="text-2xl mx-1 text-black font-extrabold">
-                    :
-                  </div>
-                  <div className=" mx-1 p-2 text-black rounded-lg">
-                    <div className="font-mono leading-none" x-text="hours">
-                    {timeLeft.hours}
-                    </div>
-                    <div className="font-mono uppercase text-sm leading-none">
-                      Hours
-                    </div>
-                  </div>
-                  <div className="text-2xl mx-1 text-black font-extrabold">
-                    :
-                  </div>
-                  <div className=" mx-1 p-2 text-black rounded-lg">
-                    <div className="font-mono leading-none" x-text="minutes">
-                    {timeLeft.minutes}
-                    </div>
-                    <div className="font-mono uppercase text-sm leading-none">
-                      Minutes
-                    </div>
-                  </div>
-                  <div className="text-2xl mx-1 text-black font-extrabold">
-                    :
-                  </div>
-                  <div className=" mx-1 p-2  text-black rounded-lg">
-                    <div className="font-mono leading-none" x-text="seconds">
-                    {timeLeft.seconds}
-                    </div>
-                    <div className="font-mono uppercase text-sm leading-none">
-                      Seconds
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div className="text-5xl text-center flex w-full items-center justify-start">
+            <div className=" mx-1 p-2 text-black rounded-lg">
+              <h1 className="font-mono leading-none">{timeLeft.days}</h1>
+              <p className="font-mono uppercase text-sm leading-none">Days</p>
+            </div>
+            <p className="text-2xl mx-1 text-black font-extrabold">:</p>
+            <div className=" mx-1 p-2 text-black rounded-lg">
+              <h1 className="font-mono leading-none">{timeLeft.hours}</h1>
+              <p className="font-mono uppercase text-sm leading-none">Hours</p>
+            </div>
+            <p className="text-2xl mx-1 text-black font-extrabold">:</p>
+            <div className=" mx-1 p-2 text-black rounded-lg">
+              <h1 className="font-mono leading-none">{timeLeft.minutes}</h1>
+              <p className="font-mono uppercase text-sm leading-none">
+                Minutes
+              </p>
+            </div>
+            <p className="text-2xl mx-1 text-black font-extrabold">:</p>
+            <div className=" mx-1 p-2  text-black rounded-lg">
+              <h1 className="font-mono leading-none">{timeLeft.seconds}</h1>
+              <p className="font-mono uppercase text-sm leading-none">
+                Seconds
+              </p>
             </div>
           </div>
           <div className="flex py-8">
